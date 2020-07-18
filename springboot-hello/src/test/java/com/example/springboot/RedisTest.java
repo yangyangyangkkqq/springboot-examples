e205e0e425f9283b86c5da61a14ad22cf037ae8a
yangkangquan <1061043418@qq.com>
2020-07-18 20:43:10 +0800

    提交

--- springboot-hello/src/test/java/com/example/springboot/RedisTest.java
+++ springboot-hello/src/test/java/com/example/springboot/RedisTest.java
@@ -1,13 +1,19 @@
 package com.example.springboot;
 
+import com.example.springboot.config.SpringUtils;
 import com.example.springboot.entity.User;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.data.redis.connection.RedisServer;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.thymeleaf.spring5.context.SpringContextUtils;
+
+import java.util.concurrent.TimeUnit;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -17,10 +23,14 @@ public class RedisTest {
     @Test
     public void redis(){
         redisTemplate.opsForValue().set("key1","value1");
-        System.out.println(redisTemplate.opsForValue().get("key1"));
+//        System.out.println(redisTemplate.opsForValue().get("key1"));
+//
+//        User user = new User("超人", 20);
+//        redisTemplate.opsForValue().set(user.getName(), user);
+//        System.out.println(redisTemplate.opsForValue().get("超人"));
 
-        User user = new User("超人", 20);
-        redisTemplate.opsForValue().set(user.getName(), user);
-        System.out.println(redisTemplate.opsForValue().get("超人"));
+        redisTemplate.opsForValue().set("lockKey", 1, 10,TimeUnit.SECONDS);
+        System.out.println(redisTemplate.opsForValue().get("lockKey"));
+
     }
 }
